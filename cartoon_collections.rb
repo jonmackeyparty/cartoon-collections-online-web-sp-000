55dd6c6aee0e14013cbabef8a0bc04f6da6a7880
Jon Mackey <jon@jonmackey.party>
2018-11-20 21:53:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(planeteer_calls)
    planeteer_calls.map{|calls| "#{calls.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls_long)
   # Your code here
 end
 
