fbf80429e6701e24fc066a941352c00740149d03
Jon Mackey <jon@jonmackey.party>
2018-11-20 22:41:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,5 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
   cheddar_cheese.find{|cheeses| cheeses.include?("cheddar" || "gouda" || "camembert")}
 end
